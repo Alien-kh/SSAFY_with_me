95f70c1fa6f1e7ac368a0e769b6101d7fd7e9379
hyeeun <ha7988@naver.com>
2025-03-07 00:57:19 +0900

    0306

--- he_Park/2진수_8진수.py
+++ he_Park/2진수_8진수.py
@@ -1,18 +1,10 @@
-# 시간초과~
-# 2진수를 8진수로 변환
-num = input()
-# 먼저 2진수 -> 10진수로 변환
-num = num[::-1]
-sum_v = 0
-for i in range(len(num)):
-    x = int(num[i]) * 2**i
-    sum_v += x
-# 10진수를 8진수로 변환
-x = ''
-share = sum_v
-while share:
-    remain = share % 8  # 나머지
-    share //= 8  # 몫
-    x += str(remain)
-
-print(x[::-1])  # 나머지를 거꾸로 읽으면 8진수가 됨
+import sys
+N = sys.stdin.readline().strip()
+while True:
+    if len(N) % 3 != 0:
+        N = '0' + N
+    else:
+        break
+for i in range(0, len(N), 3):
+    a, b, c = int(N[i]), int(N[i+1]), int(N[i+2])
+    print(a*2**2 + b*2**1 + c*2**0, end='')
